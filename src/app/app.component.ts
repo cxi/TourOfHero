43538552e8868685867868d729238a51c7884c6d
xc <xichao33@hotmail.com>
2017-05-15 16:43:27 -0400

    git push origin master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import {Component} from '@angular/core';
 @Component({
     selector:'app-root',
     template:`
-    <h1>{{title}}</h1>
-    <my-hero></my-hero>
+    <h1>{{title}}</h1> 
+    <a routerLink="/heroes">Heroes</a>
+    <router-outlet></router-outlet>
     `
 })
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './Heroes/hero-detail.component';
@@ -17,6 +18,9 @@ import { HeroService } from './Heroes/hero.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot([
+      {path: 'heroes',component:HeroesComponent}
+    ]),
     HttpModule
   ],
   providers: [HeroService],
